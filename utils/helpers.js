37f958a4d99456f27b1e9550377499d72bcdf043
hannybear88 <hannahkchung88@gmail.com>
2023-01-12 22:56:32 -0800

    fixed where date now shows in post

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,8 +2,8 @@ module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     if (date){
-      return date.toLocaleDateString();
-
+      // return date.toLocaleDateString();
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
     } else {
       return 'error'
     }
